c78346c770b5f3b7583a5d323f2a7f402cb0ec2f
Fardan Akhter <fardan@pumpjackdataworks.com>
2020-06-08 22:41:42 +0500

    file rename

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 xcodebuild -archivePath $PWD/build/Artistry.xcarchive \
-            -exportOptionsPlist Artistry-iOS/Artistry/exportOptions.plist \
+            -exportOptionsPlist Artistry/Artistry/exportOptions.plist \
             -exportPath $PWD/build \
             -allowProvisioningUpdates \
             -exportArchive | xcpretty
