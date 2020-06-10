b45c80e55b90de4322a71f5f14da16efc927315e
Fardan Akhter <fardan@pumpjackdataworks.com>
2020-06-10 20:07:40 +0500

    appstore connect api key and issuer id replaced with user name and password

--- .github/scripts/publish_testflight.sh
+++ .github/scripts/publish_testflight.sh
@@ -2,4 +2,8 @@
 
 set -eo pipefail
 
-xcrun altool --upload-app -t ios -f build/Artistry.ipa -u "$APPLEID_USERNAME" -p "$APPLEID_PASSWORD" --verbose
+#xcrun altool --upload-app -t ios -f build/Artistry.ipa -u "$APPLEID_USERNAME" -p "$APPLEID_PASSWORD" --verbose
+
+xcrun altool --upload-app -t ios -f build/Artistry.ipa --apiKey "WDSA6776SN" --apiIssuer "29b6ed58-24c0-4348-ac0a-f6a29c2ccd67" > /dev/null
+
+#xcrun altool --upload-app -f $APPFILE --apiKey $ASCKEY --apiIssuer $ASCID > /dev/null
