b7a1263a3530e9723d2a9b4913db2db59cd02595
Fardan Akhter <>
2020-06-11 17:30:38 +0500

    AWWD removed

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -4,12 +4,6 @@ set -eo pipefail
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Appstore_Dist_profile.mobileprovision ./.github/secrets/Appstore_Dist_profile.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/cert_dist.p12 ./.github/secrets/cert_dist.p12.gpg
 
-
-## These are for crendential less testflght upload ##
-
-#Apple WorldWide Developer Relation Authority Certificate
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/AWWD.cer ./.github/secrets/AWWD.cer.gpg
-
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 cp ./.github/secrets/Appstore_Dist_profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Appstore_Dist_profile.mobileprovision
@@ -17,10 +11,6 @@ cp ./.github/secrets/Appstore_Dist_profile.mobileprovision ~/Library/MobileDevic
 
 security create-keychain -p "" build.keychain
 security import ./.github/secrets/cert_dist.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
-
-# Import Apple Worldwide Developer Relations Certification Authority certificate
-#security import ./.github/secrets/AWWD.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-#
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
