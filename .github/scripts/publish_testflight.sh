30e82b25072c29a27049b57e73cd382423b6fc1c
Fardan Akhter <fardan@pumpjackdataworks.com>
2020-06-11 15:31:41 +0500

    appstore connect key and AWWD cert added

--- .github/scripts/publish_testflight.sh
+++ .github/scripts/publish_testflight.sh
@@ -3,6 +3,5 @@
 set -eo pipefail
 
 #xcrun altool --upload-app -t ios -f build/Artistry.ipa -u "$APPLEID_USERNAME" -p "$APPLEID_PASSWORD" --verbose
-
-xcrun altool --upload-app -t ios -f build/Artistry.ipa --apiKey "WDSA6776SN" --apiIssuer "29b6ed58-24c0-4348-ac0a-f6a29c2ccd67"
+xcrun altool --upload-app -t ios -f build/Artistry.ipa --apiKey "UA8FRZ3WJU" --apiIssuer "29b6ed58-24c0-4348-ac0a-f6a29c2ccd67"
 
--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -4,6 +4,12 @@ set -eo pipefail
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Appstore_Dist_profile.mobileprovision ./.github/secrets/Appstore_Dist_profile.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/cert_dist.p12 ./.github/secrets/cert_dist.p12.gpg
 
+
+## These are for crendential less testflght upload ##
+
+#Apple WorldWide Developer Relation Authority Certificate
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/AWWD.cer ./.github/secrets/AWWD.cer.gpg
+
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 cp ./.github/secrets/Appstore_Dist_profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Appstore_Dist_profile.mobileprovision
@@ -12,8 +18,18 @@ cp ./.github/secrets/Appstore_Dist_profile.mobileprovision ~/Library/MobileDevic
 security create-keychain -p "" build.keychain
 security import ./.github/secrets/cert_dist.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
+# Import Apple Worldwide Developer Relations Certification Authority certificate
+security import ./.github/secrets/AWWD.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+#
+
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
 
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+
+# Import App Store Connect private keys.
+mkdir -p ~/.appstoreconnect/private_keys
+cp secrets/keys/* ~/.appstoreconnect/private_keys/
+
+ls -l ~/.appstoreconnect/private_keys/
